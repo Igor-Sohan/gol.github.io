5e7958d4e7321310b7cd0477e4bdc2171727064f
Igor <igorsohan481@gmail.com>
2023-02-18 16:07:54 +0300

    sity

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const promoBtn = document.querySelector(".promo-btn");
+
+// promoBtn.addEventListener("click", promoBtn);
+
+// function promoBtn() {
+//     if (window.pageYOffset > 0) {
+//         window.scrollBy(0, -75);
+//         setTimeout(promoBtn, 0)
+//     }
+// }
